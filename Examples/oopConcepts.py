84fca2210b62917b1395e064a824e61904e2dc1c
Dan M <dmanastu@cisco.com>
2019-10-14 12:47:36 +0300

    Exampels Basic Python

---
+++ Examples/oopConcepts.py
@@ -0,0 +1,34 @@
+# class Om:
+#     def __init__(self, nume, varsta):
+#         """Construnctor definition"""
+#         self.nume = nume
+#         self.varsta = varsta
+#
+#     def print_age(self):
+#         """Sample method to print age"""
+#         print(self.nume + " are " + str(self.varsta) + " ani ")
+#
+#
+# first = Om("Adam", 30)
+# second = Om("Eva", 29)
+#
+# first.print_age()
+# second.print_age()
+
+
+# class Om:
+#     def __init__(self, nume, varsta):
+#         """Construnctor definition"""
+#         self.nume = nume
+#         self.varsta = varsta
+#
+#     def print_age_method(self):
+#         """Sample of method to print age"""
+#         print(self.nume + " are " + str(self.varsta) + " ani ")
+#
+#     @staticmethod
+#     def print_age_static_method(nume, varsta):
+#         print(nume + " are " + str(varsta) + " ani ")
+#
+# Om.print_age_static_method("Adam", "30")
+# Om.print_age_static_method("Eva", "28")
